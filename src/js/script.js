907fae7d93819b7d6b46066db61c418d98ba247b
William James <william.james.pj@gmail.com>
2021-07-15 18:11:34 -0300

    Create project

---
+++ src/js/script.js
@@ -0,0 +1,50 @@
+// Form save email
+const form = document.querySelector('[name="form-email"]');
+form.addEventListener("submit", saveData);
+
+function saveData(event) {
+  event.preventDefault();
+
+  const res = localStorage.getItem("data_form");
+
+  let objeto = [];
+
+  if (res) objeto = JSON.parse(res);
+
+  console.log(objeto);
+
+  const email = event.currentTarget.email.value;
+
+  objeto.push({ email });
+
+  localStorage.setItem("data_form", JSON.stringify(objeto));
+}
+
+// Nav link
+const menuItems = document.querySelectorAll('nav a[href^="#"]');
+function getScrollTopByHref(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop - 60;
+}
+function scrollToPosition(to) {
+  window.scroll({
+    top: to,
+    behavior: "smooth",
+  });
+}
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const to = getScrollTopByHref(event.currentTarget);
+  scrollToPosition(to);
+}
+menuItems.forEach((item) => {
+  item.addEventListener("click", scrollToIdOnClick);
+});
+
+// Nav shadow
+function scrollNav() {
+  const nav = document.getElementById("nav");
+  if (this.scrollY >= 80) nav.classList.add("scroll-nav");
+  else nav.classList.remove("scroll-nav");
+}
+window.addEventListener("scroll", scrollNav);
